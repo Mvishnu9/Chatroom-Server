6efcef37ccc5c51dd8fcbabcdbaea9e6260a03a7
Vishnu M <mvishnu9@gmail.com>
2018-08-17 23:13:58 +0530

    Added list, add functions to room.

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -8,8 +8,7 @@ public class Server
 {    
     static Vector <ClientHandler> ClientList = new Vector<>();
     static Vector <Room> RoomList = new Vector<>();
-    
-    
+        
     static int ClientCount = 0;
     
     private Socket socket = null;
@@ -175,7 +174,7 @@ class ClientHandler
     
     public void Exec() 
     {
-        String received;
+        String receivedT;
         String rname = thisCL.room;
 
         Room thisRoom = RoomList.firstElement();
@@ -194,11 +193,13 @@ class ClientHandler
         {
             try
             {
-                received = dis.readUTF();
+                receivedT = dis.readUTF();
+                StringTokenizer st = new StringTokenizer(receivedT);
+                String tok = st.nextToken();
+                
+                System.out.println(name+": "+receivedT);
 
-                System.out.println(name+": "+received);
-
-                if(received.equals("logout"))
+                if(tok.equals("logout"))
                 {
                     for (ClientHandler cl : thisRoom.ClientList)
                     {
@@ -209,9 +210,65 @@ class ClientHandler
                     s.close();
                     break;
                 }
+                else if(tok.equalsIgnoreCase("exit"))
+                {
+                    thisCL.room = "lobby";
+                    thisRoom.RemoveClient(thisCL);
+                    for (ClientHandler cl : thisRoom.ClientList)
+                    {
+                        cl.dos.writeUTF(name+" has left the room");
+                    }
+                    RoomLobby();
+                    return;                    
+                }
+                else if(tok.equalsIgnoreCase("add"))
+                {
+                    if(st.countTokens() < 1)
+                    {
+                        dos.writeUTF("Invalid Command, Please Retry");
+                        continue;
+                    }
+                    String name = st.nextToken();
+                    for(ClientHandler cl: Server.ClientList)
+                    {
+                        if(name.equals(cl.GetName()))
+                        {
+                            if(cl.room.equals("lobby"))
+                            {
+                                cl.room = thisCL.room;
+                                thisRoom.AddClient(cl);
+                                for (ClientHandler Rcl : thisRoom.ClientList)
+                                {
+                                    Rcl.dos.writeUTF(name+" has been added to the room");
+                                }
+                                continue;                               
+                            }
+                            else
+                            {
+                                dos.writeUTF("The Client associated with the name is already part of another Chat room");
+                                continue;
+                            }
+                        }
+                                
+                    }
+                    dos.writeUTF("Client with associated name doesnt exist.");
+                    continue;
+                   
+                    
+                }
+                else if(tok.equalsIgnoreCase("list"))
+                {
+                    dos.writeUTF("------------------------------------------------------------");
+                    for(ClientHandler cl : thisRoom.ClientList)
+                    {
+                        dos.writeUTF(cl.GetName());
+                    }
+                    dos.writeUTF("------------------------------------------------------------");
+                    continue;                    
+                }
                 for (ClientHandler cl : thisRoom.ClientList)
                 {
-                    cl.dos.writeUTF(name+" : "+received);
+                    cl.dos.writeUTF(name+" : "+receivedT);
                 }
 
             }
@@ -242,6 +299,7 @@ class ClientHandler
         }
     }
     
+    
     void RoomLobby()
     {
         class Lobby implements Runnable
@@ -251,39 +309,74 @@ class ClientHandler
             {
                 CL = cl;
             }
+            public void DisplayInstructions(DataOutputStream dos)
+            {
+                try
+                {
+                    dos.writeUTF("Select one of the following commands:");
+                    dos.writeUTF("1) LIST - Displays list of currenlty active chat rooms");
+                    dos.writeUTF("2) CREATE <Name of chat room> - Create a new chat room");
+                    dos.writeUTF("3) ENTER <Name of chat room> - Enter an existing chat room");
+                    dos.writeUTF("4) LOGOUT - To logout of the application");
+                    dos.writeUTF("------------------------------------------------------------");
+                }
+                catch(IOException i)
+                {
+                    i.printStackTrace();
+                }
+                return;
+
+            }
             public void run()
             {        
                 while(room.equals("lobby"))
                 {
                     try
                     {
-                        dos.writeUTF("Select one of the following commands:");
-                        dos.writeUTF("1) LIST - Displays list of currenlty active chat rooms");
-                        dos.writeUTF("2) CREATE - Create a new chat room and enter it");
-                        dos.writeUTF("3) ENTER <Name of chat room> - Enter an existing chat room");
+                        DisplayInstructions(dos);
                         String received;
                         received = dis.readUTF();
                         StringTokenizer st = new StringTokenizer(received);
                         String tok = st.nextToken();
-                        if(tok.equals("LIST"))
+                        if(tok.equalsIgnoreCase("LIST"))
                         {
-                           dos.writeUTF("Typed LIST");
+                           dos.writeUTF("------------------------------------------------------------");
                            for(Room room: RoomList)
                            {
-                               dos.writeUTF(room.GetName());                      
+                               String test = "lobby";
+                               if(!test.equalsIgnoreCase(room.GetName()))
+                               {
+                                   dos.writeUTF(room.GetName());
+                               }                      
                            }
+                           dos.writeUTF("------------------------------------------------------------");
                         }
-                        else if(tok.equals("CREATE"))
+                        else if(tok.equalsIgnoreCase("CREATE"))
                         {
-                            dos.writeUTF("Please Enter Name of the new Room");
-                            String newrname = dis.readUTF();
+                            if(st.countTokens() < 1)
+                            {
+                                dos.writeUTF("Invalid Command, Please Try again");
+                                continue;
+                            }
+                            String newrname = st.nextToken();
+                            for(Room check: RoomList)
+                            {
+                                if(newrname.equalsIgnoreCase(check.GetName()))
+                                {
+                                    dos.writeUTF("Name Already Taken");
+                                    continue;
+                                }
+                            }
                             Room newroom = new Room(newrname);
-                            //newroom.AddClient(CL);
                             RoomList.add(newroom);
-
                         }
-                        else if(tok.equals("ENTER"))
+                        else if(tok.equalsIgnoreCase("ENTER"))
                         {
+                            if(st.countTokens() < 1)
+                            {
+                                dos.writeUTF("Invalid Command, Please Try again");
+                                continue;
+                            }
                             String roomname = st.nextToken();
                             CL.room = roomname;
                             Room thisRoom = RoomList.firstElement();
@@ -299,7 +392,7 @@ class ClientHandler
                             }
                             thisRoom.AddClient(CL);                           
                         }
-                        else if(tok.equals("logout"))
+                        else if(tok.equalsIgnoreCase("logout"))
                         {
                             for (ClientHandler cl : Server.ClientList)
                             {
@@ -309,6 +402,10 @@ class ClientHandler
                             isloggedin = false;
                             s.close();
                             return;                           
+                        }
+                        else
+                        {
+                            dos.writeUTF("Invalid command, Please Enter one of the following commands");
                         }
                     }
                     catch(IOException i)
@@ -322,6 +419,5 @@ class ClientHandler
         }
         Thread t = new Thread(new Lobby(thisCL));
         t.start();
-    }
-    
+    }    
 }
